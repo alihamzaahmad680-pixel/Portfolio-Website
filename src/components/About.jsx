9d7e5fb67c7aea8663670e951f4e0c8f2bf97f28
alihamzaahmad680-pixel <YOUR_GITHUB_EMAIL>
2026-07-29 18:41:56 +0500

    Updated project

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
               <p className="text-gray-300 mb-6">
                 I enjoy solving complex problems and creating seamless
                 end-to-end web applications that provide great user
-                experiences.and.
+                experiences.
               </p>
 
               {/* CARDS */}
